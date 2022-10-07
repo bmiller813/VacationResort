331f327988d64f1ff1cbcd80655f5228d6ef7694
Bryan <bmiller@tb.yearup.org>
2022-10-07 16:24:08 -0400

    Rough finish of JS code calculate.js, added outputs to booking.html not finished with script

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -9,30 +9,31 @@ let rangeStart = new Date("june 1 " + y); //range start
 let rangeEnd = new Date("august 31 " + y); //range end
 
 //RATE
-let roomRate = 150
-let daysVisit = 1
-let visitorNum = 1
+let roomRate = 150;
+let daysVisit = 1;
+let visitorNum = 1;
 
 let hasQueen = false;
 let hasKing = false;
 let hasTwoBed = false;
+let container = "queen";
 
 document.getElementById("queen").addEventListener("click", (e) => {
     console.log("QUEEN: " + e.target.checked);
     container = "queen";
-    visitorNum = 5
+    visitorNum = 5;
 });
 
 document.getElementById("king").addEventListener("click", (e) => {
     console.log("KING: " + e.target.checked);
     container = "king";
-    visitorNum = 2
+    visitorNum = 2;
 });
 
 document.getElementById("twoBed").addEventListener("click", (e) => {
     console.log("TWOBEDS: " + e.target.checked);
     container = "twoBed";
-    visitorNum = 6
+    visitorNum = 6;
 });
 
 
@@ -41,23 +42,29 @@ document.getElementById("twoBed").addEventListener("click", (e) => {
 function getRoomRate() {
     //HTML VARIABLES
     hasQueen = document.getElementById("queen").checked;
-    hasKing = document.getElementById("queen").checked;
-    hasTwoBed = document.getElementById("queen").checked;
+    hasKing = document.getElementById("king").checked;
+    hasTwoBed = document.getElementById("twoBed").checked;
 
     daysVisit = document.getElementById("daysVisit").value;
     adults = document.getElementById("adultNum").value;
     kids = document.getElementById("kidNum").value;
-    visitorNum = Number(adults + kids);
+    visitorNum = Number(adults) + Number(kids);
+
+    noDiscount = document.getElementById("noDiscount").checked;
+    seniorDiscount = document.getElementById("seniorDiscount").checked;
+    militaryDiscount = document.getElementById("militaryDiscount").checked;
     //OUTPUT VARIABLES
     let basePrice = 0;
-    let tax = 0;
-    let discountPrice = 0;
+    let discount = 1;
+    let discountOff = 0;
     let total = 0;
     const TAX_RATE = 0.12;
+    let tax = 0;
 
-    basePrice = 1
-    basePrice += daysVisit * 150 //150 per day
-    
+    basePrice = 1;
+    basePrice += daysVisit * 150; //150 per day
+
+
     //VALIDATE
     if (daysVisit < 1 || daysVisit > 28) {
         console.log("MUST CHOOSE DURATION OF VISIT");
@@ -74,20 +81,42 @@ function getRoomRate() {
         console.log("TOO MANY OCCUPANTS");
     }
 
-    //PRICING PER ROOM
+    //BASE PRICING PER ROOM
     if ("queen" == container && d >= rangeStart && d < rangeEnd) {
-        basePrice += 100
+        basePrice += 100;
     }
     if ("king" == container && d >= rangeStart && d < rangeEnd) {
-        basePrice += 100
+        basePrice += 100;
     }
     if ("twoBed" == container && d >= rangeStart && d < rangeEnd) {
-        basePrice += 200
-    }else if("twoBed" == container){
-        basePrice += 60
+        basePrice += 200;
+    } else if ("twoBed" == container) {
+        basePrice += 60;
     }
 
     //DISCOUNTS
-    
+    if (noDiscount) {
+        discount = 0;
+        console.log("No Changes");
+    } else if (seniorDiscount) {
+        discount = 0.1;
+        console.log("senior discount babyyy");
+    }
+    else if (militaryDiscount) {
+        discount = 0.2;
+        console.log("Thank You For Your Service");
+    }
 
-};
+
+    discountOff = basePrice*discount;
+    totalBT = (basePrice - discountOff);
+    tax = totalBT * TAX_RATE;
+    total = totalBT - tax;
+
+    document.getElementById("totalOutput").innerHTML = basePrice;
+    document.getElementById("discountOutput").innerHTML = discount;
+    document.getElementById("discountTotalOutput").innerHTML = totalBT;
+    document.getElementById("taxOutput").innerHTML = tax;
+    document.getElementById("finalOutput").innerHTML = total;
+}
+document.getElementById("submitBtn").addEventListener("click", getRoomRate);
